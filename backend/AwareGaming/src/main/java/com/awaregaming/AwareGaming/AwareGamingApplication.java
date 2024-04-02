d831906db2ce9626bb64f72ad190d18235990a0f
rubenledesma10 <rubenledesma89@gmail.com>
2024-04-02 18:00:46 -0300

    push rama main

---
+++ backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/AwareGamingApplication.java
@@ -0,0 +1,13 @@
+package com.awaregaming.AwareGaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwareGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwareGamingApplication.class, args);
+	}
+
+}
