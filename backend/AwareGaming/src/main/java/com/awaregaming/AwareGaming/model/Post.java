a9cdd3810aea07f479bc76460f1f5632fcddddfc
rubenledesma10 <rubenledesma89@gmail.com>
2024-04-04 14:38:24 -0300

    esperar a que hans me mande user para terminar

--- backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/model/Post.java
+++ backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/model/Post.java
@@ -1,24 +1,33 @@
 package com.awaregaming.AwareGaming.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-@Entity
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "posts")
 public class Post {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idPost;
     private String title;
     private String description;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime created_at;
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
     private LocalDateTime updated_at;
     private int userId;
 
