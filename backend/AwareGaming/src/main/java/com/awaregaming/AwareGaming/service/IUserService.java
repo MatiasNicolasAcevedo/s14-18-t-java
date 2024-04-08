058ac6a8f0de14a20264b2650edc2316d003a443
HansUrpay <hansurpayh@gmail.com>
2024-04-08 00:24:47 -0500

    feat: user entity

---
+++ backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/service/IUserService.java
@@ -0,0 +1,24 @@
+package com.awaregaming.AwareGaming.service;
+
+import com.awaregaming.AwareGaming.dto.UserRequestDto;
+import com.awaregaming.AwareGaming.dto.UserResponseDto;
+import com.awaregaming.AwareGaming.exceptions.UserUpdateException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface IUserService extends UserDetailsService {
+
+    @Transactional(readOnly = true)
+    List<UserResponseDto> getAllUsers();
+
+    @Transactional(readOnly = true)
+    ResponseEntity<UserResponseDto> getUser(int id) throws UsernameNotFoundException;
+
+    ResponseEntity<String> updateUser(int id, UserRequestDto userRequestDto) throws UserUpdateException;
+
+    ResponseEntity<String> deleteUser(int id) throws UsernameNotFoundException;
+}
