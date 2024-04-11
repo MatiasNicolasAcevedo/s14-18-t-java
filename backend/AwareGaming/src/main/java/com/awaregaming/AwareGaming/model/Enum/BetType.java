a8a4b9beb16e444c01cc5ff7d2a2de871ac9db7e
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-04-11 13:50:43 -0300

    feat: add Enum BetType

---
+++ backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/model/Enum/BetType.java
@@ -0,0 +1,17 @@
+package com.awaregaming.AwareGaming.model.Enum;
+
+public enum BetType {
+    RED,
+    BLACK,
+    EVEN,
+    ODD,
+    NUMBER,
+    LOWER_NUMBERS,
+    HIGH_NUMBERS,
+    FIRST_DOZEN,
+    SECOND_DOZEN,
+    THIRD_DOZEN,
+    FIRST_ROW,
+    SECOND_ROW,
+    THIRD_ROW
+}
