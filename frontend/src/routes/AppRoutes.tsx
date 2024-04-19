f2eb7f412044be5d3a0e6b10998a949a0893dc34
Aracely Cruz <yuki_oh91@hotmail.com>
2024-04-19 14:05:03 -0600

    feat: Maquetado del dashboard

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,6 +2,8 @@ import { useRoutes } from 'react-router-dom';
 import { RegisterPage } from '@/pages/RegisterPage';
 import LandingPage from '@/pages/LandingPage';
 import LoginPage from '@/pages/LoginPage';
+import DashboardPage from '@/pages/DashboardPage';
+import ProfilePage from '@/pages/ProfilePage';
 
 export const AppRoutes = () => {
 	const routes = [
@@ -16,6 +18,21 @@ export const AppRoutes = () => {
 		{
 			path: '/login',
 			element: <LoginPage />,
+		},
+		{
+			path: '/dashboard',
+			element: <DashboardPage />,
+			children: [
+				{
+					path: '',
+					redirect: 'profile',
+					pathmatch: 'full',
+				},
+				{
+					path: 'profile',
+					element: <ProfilePage />,
+				},
+			],
 		},
 	];
 
