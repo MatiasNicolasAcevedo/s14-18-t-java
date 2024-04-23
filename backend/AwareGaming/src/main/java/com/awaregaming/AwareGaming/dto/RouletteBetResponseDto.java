f1d9f1b97fb885819e2c2c864ef4bf177ac1640c
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-04-23 17:33:27 -0300

    feat: add data colorNumber and winingBet

--- backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/dto/RouletteBetResponseDto.java
+++ backend/AwareGaming/src/main/java/com/awaregaming/AwareGaming/dto/RouletteBetResponseDto.java
@@ -1,10 +1,14 @@
 package com.awaregaming.AwareGaming.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RouletteBetResponseDto {
     private String result;
     private Integer newCredit;
     private Integer winningNumber;
+    private String numberColor;
+    private Integer winningAmount;
 }
